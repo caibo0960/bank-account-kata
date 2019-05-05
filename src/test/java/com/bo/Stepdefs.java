6c51cc071e54cac27fb86929ba1d5a1509e3f1ee
CAI Bo <caibo0960@gmail.com>
2019-05-05 19:07:53 +0200

    Declare step definitions.

--- src/test/java/com/bo/Stepdefs.java
+++ src/test/java/com/bo/Stepdefs.java
@@ -4,7 +4,46 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
+import cucumber.runtime.java.StepDefAnnotation;
+
+import java.math.BigDecimal;
+
 import static org.junit.Assert.*;
 
 public class Stepdefs {
+
+    @Given("^I am a client having a bank account of (-?\\d+)$")
+    public void createNewBankAccount(BigDecimal balance) {
+
+    }
+
+    @When("^I make a deposit of (-?\\d+)$")
+    public void makeDeposit(BigDecimal amount) {
+
+    }
+
+    @When("^I make a withdrawal of (-?\\d+)$")
+    public void makeWithdrawal(BigDecimal amount) {
+
+    }
+
+    @Then("^My account should have (-?\\d+)$")
+    public void checkAccount(BigDecimal amount) {
+
+    }
+
+    @Then("^Impossible operation should raise$")
+    public void checkError() {
+
+    }
+
+    @When("^I want see my account operation history$")
+    public void checkOperationHistory() {
+
+    }
+
+    @Then("^The account operation history should be print as follows$")
+    public void checkOperationHistoryResult() {
+
+    }
 }
